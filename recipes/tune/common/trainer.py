251fb0216854d59c204f9612cb4f70c44167e3c3
Pawel Garbacki <pgarbacki@users.noreply.github.com>
2023-08-17 17:26:58 -0700

    Sync with internal changes (#13)

--- recipes/tune/common/trainer.py
+++ recipes/tune/common/trainer.py
@@ -10,8 +10,12 @@ from transformers import AutoTokenizer
 from omegaconf import DictConfig
 
 
-def train(config: DictConfig, tokenizer: AutoTokenizer, dataset: DatasetDict,
-          model: torch.nn.Module) -> torch.nn.Module:
+def train(
+    config: DictConfig,
+    tokenizer: AutoTokenizer,
+    dataset: DatasetDict,
+    model: torch.nn.Module,
+) -> torch.nn.Module:
     """
     Fine tunes a summarization model.
 
@@ -24,22 +28,30 @@ def train(config: DictConfig, tokenizer: AutoTokenizer, dataset: DatasetDict,
     Returns:
         trained model.
     """
-    per_device_macro_batch_size = config.model.batch_size // dist.get_world_size(
+    per_device_macro_batch_size = config.model.batch_size // dist.get_world_size()
+    gradient_accumulation_steps = (
+        per_device_macro_batch_size // config.model.micro_batch_size
     )
-    gradient_accumulation_steps = per_device_macro_batch_size // config.model.micro_batch_size
-    print(f"per_device_train_batch_size: {config.model.micro_batch_size} "
-          f"gradient_accumulation_steps: {gradient_accumulation_steps}")
+    print(
+        f"per_device_train_batch_size: {config.model.micro_batch_size} "
+        f"gradient_accumulation_steps: {gradient_accumulation_steps}"
+    )
     deepspeed_config = config.model.get("deepspeed_config", None)
     if deepspeed_config:
         print(f"Deepspeed config: {deepspeed_config}")
     ddp_find_unused_parameters = not config.model.gradient_checkpointing
+    kwargs = {}
+    lr_scheduler_type = config.model.get("lr_scheduler_type")
+    if lr_scheduler_type:
+        kwargs["lr_scheduler_type"] = lr_scheduler_type
+    warmup_steps = config.model.get("warmup_steps", 10)
     trainer = transformers.Trainer(
         model=model,
         train_dataset=dataset,
         args=transformers.TrainingArguments(
             per_device_train_batch_size=config.model.micro_batch_size,
             gradient_accumulation_steps=gradient_accumulation_steps,
-            warmup_steps=10,
+            warmup_steps=warmup_steps,
             num_train_epochs=config.model.epochs,
             learning_rate=config.model.learning_rate,
             bf16=config.model.get("bf16", False),
@@ -49,11 +61,17 @@ def train(config: DictConfig, tokenizer: AutoTokenizer, dataset: DatasetDict,
             deepspeed=deepspeed_config,
             gradient_checkpointing=config.model.gradient_checkpointing,
             ddp_find_unused_parameters=ddp_find_unused_parameters,
+            group_by_length=False,
             # max_steps=1,
             report_to=None,
+            **kwargs,
         ),
-        data_collator=transformers.DataCollatorForLanguageModeling(tokenizer,
-                                                                   mlm=False),
+        data_collator=transformers.DataCollatorForSeq2Seq(
+            tokenizer, pad_to_multiple_of=8, return_tensors="pt", padding=True
+        ),
+        # data_collator=transformers.DataCollatorForLanguageModeling(
+        #     tokenizer, mlm=False
+        # ),
     )
     model.config.use_cache = False
     trainer.train(resume_from_checkpoint=False)
