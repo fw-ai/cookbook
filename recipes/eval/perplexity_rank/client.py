ec081eca2c47bb653c33dae32528ef6cba8fcc62
Pawel Garbacki <pawel@fireworks.ai>
2023-09-04 21:42:19 +0000

    Added sentence piece ranking

--- recipes/eval/perplexity_rank/client.py
+++ recipes/eval/perplexity_rank/client.py
@@ -13,6 +13,8 @@ from omegaconf import DictConfig
 from recipes.common.env import env
 from recipes.common.peft import load_inference_model
 from recipes.common.tokenizer import load_tokenizer
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 class Client(abc.ABC):
@@ -150,3 +152,20 @@ class FireworksClient(Client):
             prompt=prompt,
         )
         return completion.choices[0].text
+
+
+class SentenceTransformerClient(Client):
+    def __init__(self, config: DictConfig) -> None:
+        super().__init__(config)
+        device = env().device
+        self._model = SentenceTransformer(config.model.name, device=device)
+
+    def perplexity(self, prompt: str, completion: str) -> float:
+        prompt_embedding = self._model.encode(prompt, show_progress_bar=False)
+        completion_embedding = self._model.encode(completion, show_progress_bar=False)
+        similarity = cosine_similarity([prompt_embedding], [completion_embedding])[0][0]
+        similarity = min(similarity, 1.0)
+        return 1.0 - similarity
+
+    def completion(self, prompt: str) -> str:
+        raise NotImplementedError("completion is not supported by embedding models")
