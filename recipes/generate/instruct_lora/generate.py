3231a67f23fa36c3a33d5937bc8c6fe295316f72
Pawel Garbacki <pawel@fireworks.ai>
2023-10-02 19:34:59 +0000

    Enable streaming generator

--- recipes/generate/instruct_lora/generate.py
+++ recipes/generate/instruct_lora/generate.py
@@ -32,6 +32,13 @@ def _patch(config: DictConfig) -> None:
 
         replace_llama_attn_with_flash_attn()
 
+    # Monkey-patch nn.Linear to disable initialization since the weights will be
+    # overridden anyway.
+    def _no_op_reset(self):
+        pass
+
+    torch.nn.Linear.reset_parameters = _no_op_reset
+
 
 def _extract_response(config: DictConfig, output: str) -> str:
     """
@@ -67,10 +74,14 @@ def _generate(
     prompt = config.prompt
     inputs = tokenizer(prompt, return_tensors="pt")
     inputs = {k: v.to(device) for k, v in inputs.items()}
+    streamer = TextStreamer(
+        tokenizer, skip_special_tokens=False, spaces_between_special_tokens=False
+    )
     outputs = model.generate(
         input_ids=inputs["input_ids"],
         attention_mask=inputs["attention_mask"],
         max_new_tokens=config.max_new_tokens,
+        streamer=streamer,
     )
     outputs = outputs.squeeze(0)
     try:
@@ -79,7 +90,9 @@ def _generate(
     except ValueError:
         ...
     outputs = outputs.unsqueeze(0)
-    decoded = tokenizer.batch_decode(outputs, skip_special_tokens=False)[0]
+    decoded = tokenizer.batch_decode(
+        outputs, skip_special_tokens=False, spaces_between_special_tokens=False
+    )[0]
     return _extract_response(config, decoded)
 
 
@@ -96,11 +109,11 @@ def _app(config: DictConfig) -> None:
     env = init_env()
     tokenizer = load_tokenizer(config.model, add_eos_token=False)
     model = load_inference_model(config)
-    response = _generate(config, tokenizer, model, env.device)
-    RED = "\033[1;31m"
-    GREEN = "\033[1;32m"
-    RESET = "\033[0m"
-    print(f"{GREEN}text:{RESET} {config.input}\n{RED}answer:{RESET} {response}")
+    _response = _generate(config, tokenizer, model, env.device)
+    # RED = "\033[1;31m"
+    # GREEN = "\033[1;32m"
+    # RESET = "\033[0m"
+    # print(f"{GREEN}text:{RESET} {config.input}\n{RED}answer:{RESET} {response}")
 
 
 if __name__ == "__main__":
